44be88049a38d87bc778702fdd0e10af3c12779c
ManavGahlot <manavgahlot.in@gmail.com>
2025-12-26 17:25:06 +0530

    Add Google Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,39 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import localFont from "next/font/local";
 import "./globals.css";
+// 1. Import the Google Analytics component
+import { GoogleAnalytics } from '@next/third-parties/google'
 
-const inter = Inter({ subsets: ["latin"] });
+const geistSans = localFont({
+  src: "./fonts/GeistVF.woff",
+  variable: "--font-geist-sans",
+  weight: "100 900",
+});
+const geistMono = localFont({
+  src: "./fonts/GeistMonoVF.woff",
+  variable: "--font-geist-mono",
+  weight: "100 900",
+});
 
 export const metadata: Metadata = {
-  title: "US Income Calculator",
-  description: "Affordability calculator",
+  title: "US Salary Needed Calculator (2025 Data)",
+  description: "Calculate the salary needed to rent an apartment in any US zip code based on HUD 2025 data.",
 };
 
 export default function RootLayout({
   children,
-}: {
+}: Readonly<{
   children: React.ReactNode;
-}) {
+}>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+      {/* 2. Add the Component with your ID */}
+      <GoogleAnalytics gaId="G-7FWF7Z1QEJ" />
     </html>
   );
 }
